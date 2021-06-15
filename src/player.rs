f021f7a5a3a67becde1b10f98cd7dee773a75e85
Tatounee <oettam32@gmail.com>
2021-06-15 03:49:23 +0200

    Group together trait and struct about Donation

--- src/player.rs
+++ src/player.rs
@@ -110,3 +110,92 @@ impl<'de> Deserialize<'de> for Player {
         deserializer.deserialize_struct("Player", FIELDS, PlayerVisitor)
     }
 }
+
+pub trait DonationUtils {
+    fn as_u32(&self) -> u32;
+    fn is_pet(&self) -> bool;
+}
+impl DonationUtils for String {
+    type Output = String;
+
+    fn as_u32(&self) -> u32 {
+        match self.as_ref() {
+            "Barbarian" => 1,
+            "Archer" => 2,
+            "Goblin" => 3,
+            "Giant" => 4,
+            "Wall Breaker" => 5,
+            "Balloon" => 6,
+            "Wizard" => 7,
+            "Healer" => 8,
+            "Dragon" => 9,
+            "P.E.K.K.A" => 10,
+            "Baby Dragon" => 11,
+            "Miner" => 12,
+            "Electro Dragon" => 13,
+            "Yeti" => 14,
+
+            "Minion" => 101,
+            "Hog Rider" => 102,
+            "Valkyrie" => 103,
+            "Golem" => 104,
+            "Witch" => 105,
+            "Lava Hound" => 106,
+            "Bowler" => 107,
+            "Ice Golem" => 108,
+            "Headhunter" => 109,
+
+            "Super Barbarian" => 201,
+            "Super Archer" => 202,
+            "Super Giant" => 203,
+            "Sneaky Goblin" => 204,
+            "Super Wall Breaker" => 205,
+            "Rocket Balloon" => 206,
+            "Super Wizard" => 207,
+            "Inferno Dragon" => 208,
+            "Super Minion" => 209,
+            "Super Valkyrie" => 210,
+            "Super Witch" => 211,
+            "Ice Hound" => 212,
+
+            "Lightning Spell" => 301,
+            "Healing Spell" => 302,
+            "Rage Spell" => 303,
+            "Jump Spell" => 304,
+            "Freeze Spell" => 305,
+            "Clone Spell" => 306,
+            "Invisibility Spell" => 307,
+
+            "Poison Spell" => 401,
+            "Earthquake Spell" => 402,
+            "Haste Spell" => 403,
+            "Skeleton Spell" => 404,
+            "Bat Spell" => 405,
+
+            "Wall Wrecker" => 501,
+            "Battle Blimp" => 502,
+            "Stone Slammer" => 503,
+            "Siege Barracks" => 504,
+            "Log Launcher" => 505,
+
+            "Mighty Yak" => 601,
+            "Unicorn" => 602,
+            "L.A.S.S.I" => 603,
+            "Electro Owl" => 604,
+            _ => u32::MAX,
+        }
+    }
+
+    #[inline]
+    fn is_pet(&self) -> bool {
+        let id = self.as_u32();
+        id > 600 && id <= 700
+    }
+
+    #[inline]
+    fn is_super_troop(&self) -> bool {
+        let id = self.as_u32();
+        id > 200 && id <= 300
+    }
+
+}
